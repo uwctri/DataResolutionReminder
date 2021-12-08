462f95e20611430fc356735575c302f17de4dc2a
adam nunez <adam.a.nunez@gmail.com>
2021-12-08 13:09:02 -0600

    broke core cron function into two

--- DataResolutionReminder.php
+++ DataResolutionReminder.php
@@ -17,131 +17,141 @@ class DataResolutionReminder extends AbstractExternalModule {
     }
     
     /*
-     *Redcap cron for core functionality, this whole EM is kinda just a cron job
+     *Redcap cron
      */
-    public function checkForReminders($cronInfo) {
+    public function cron( $cronInfo ) {
         
         // Stash original PID, probably not needed, but docs recommend
         $originalPid = $_GET['pid'];
         
-        // Constants across all pids
-        $now = date("Y-m-d h:i");
+        // Get server http(s)
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' 
             || $_SERVER['SERVER_PORT'] == 443) ? 'https' : 'http';
         
         // Loop over every pid using this EM
         foreach($this->getProjectsWithModuleEnabled() as $pid) {
             
-            // Act like we are in that project and get settings/projectName
+            // Act like we are in that project, make a link, call core function
             $_GET['pid'] = $pid;
             $link = "{$protocol}://{$_SERVER["SERVER_NAME"]}/redcap/redcap_v".REDCAP_VERSION."/index.php?pid={$pid}";
-            $sql = 'SELECT app_title
-                    FROM redcap_projects
-                    WHERE project_id = ?';
-            $result = $this->query($sql, [$pid]);  // getProjectTitle doesn't work in crons
-            $projectName = $result->fetch_assoc()['app_title'];
-            $settings = $this->getProjectSettings();
-            $sentSetting = $settings['sent'];
-            $$updateProjectSetting = false;
-            
-            // Gather User IDs and reformat
-            $users = User::getProjectUsernames(null,false,$pid);
-            $sql = 'SELECT ui_id, username, user_email
-                    FROM redcap_user_information 
-                    WHERE username IN (?)';
-            $result = $this->query($sql,['"'.implode('","',$users).'"']); 
-            $projectUsers = [];
-            while($row = $result->fetch_assoc()){
-                $projectUsers[$row['username']] = [
-                    'id' => $row['ui_id'],
-                    'email' => $row['user_email']
-                ];
-            }
-            
-            // Gather all valid Status IDs for project ID
-            $sql = 'SELECT status_id 
-                    FROM redcap_data_quality_status 
-                    WHERE project_id = ?';
-            $result = $this->query($sql, [$pid]);
-            $statusIDs = [];
-            while($row = $result->fetch_assoc()){
-                $statusIDs[] = $row['status_id'];
-            }
-            $statusIDs = implode(',',$statusIDs);
-            
-            // Loop over the user lists and conditionally send emails
-            foreach($settings['user'] as $index => $userList) {
-                $condition = $settings['condition'][$index];
-                $days = $settings['condition'][$index];
-                $freq = $settings['frequency'][$index];
-                $sent = $settings['sent'][$index];
-                
-                if ( empty($condition) || empty($days) || empty($freq) || empty($userList) ) {
-                    continue; // We need every setting
-                }
-                
-                if ( $sent != "" && $freq == 0 ) {
-                    continue; // Send only once, skip
-                }
-                
-                if ( $sent != "" && $now > date('Y-m-d h:i', strtotime($sent . " + $freq days")) ) {
-                    continue; // Not enough time has passed to send the next reminder
-                }
-                
-                // Prep for our query to find open DQs
-                $sql = 'SELECT ts, user_id, comment
-                        FROM redcap_data_quality_resolutions 
-                        WHERE current_query_status = "OPEN" 
-                        AND user_id IN (?) AND status_id IN (?)';
-                $userIds = array_combine(array_keys($projectUsers),array_column($projectUsers, 'id'));
-                $result = [];
-                
-                // If user in the list has open data query
-                if ( $condition == 1 ) {
-                    $fUsers = array_values(array_intersect_key($userIds, array_flip($userList)));
-                    $result = $this->query($sql,['"'.implode('","',$fUsers).'"',$statusIDs]);
-                }
-                
-                // If any user on the project has an open data query
-                if ( $condition == 2 ) {
-                    $result = $this->query($sql,['"'.implode('","',array_values($userIds)).'"',$statusIDs]);
-                }
-                
-                // Check if enough time has passed sense the DQ was opened
-                $sendEmail = false;
-                while($row = $result->fetch_assoc()){
-                    if ( $now < date("Y-m-d h:i", strtotime($row['ts'] . " + $days days")) ) {
-                        $sendEmail = true;
-                        break;
-                    }
-                }
-                
-                // Send the email and set flag to save
-                if ( $sendEmail ) {
-                    foreach ( $userList as $user ) {
-                        $to = $projectUsers[$user]['email'];
-                        $from = $project_contact_email;
-                        $subject = "[REDCap] Data query reminder";
-                        $projLink = "<a link=\"$link\">\"$projectName\"</a>";
-                        $msg = "There are open data queries in the REDCap project $projLink that need to be addressed.";
-                        REDCap::email($to, $from, $subject, $msg);
-                    }
-                    $sentSetting[$index] = $now; 
-                    $updateProjectSetting = true;
-                }
-            }
-            
-            // We've flipped through all of the userLits in the project
-            // Update the project setting if anything was sent
-            if ( $updateProjectSetting ) {
-                $this->setProjectSetting("sent",$sentSetting);
-            }
+            $this->checkForReminders( $pid, $link );
         }
 
         // Put the pid back the way it was before this cron job
         // likely doesn't matter, but is good housekeeping practice
         $_GET['pid'] = $originalPid;
         return "The \"{$cronInfo['cron_description']}\" cron job completed successfully.";
+    }
+    
+    /*
+     *Core functionality, check a pid for DQ reminders and send emails
+     */
+    private function checkForReminders( $project_id, $project_link ) {
+        
+        // Gather project settings and title
+        $sql = 'SELECT app_title
+                FROM redcap_projects
+                WHERE project_id = ?';
+        $result = $this->query($sql, [$project_id]);  // getProjectTitle doesn't work in crons
+        $projectName = $result->fetch_assoc()['app_title'];
+        $settings = $this->getProjectSettings();
+        $sentSetting = $settings['sent'];
+        $updateProjectSetting = false;
+        $now = date("Y-m-d h:i");
+        
+        // Gather User IDs and reformat
+        $users = User::getProjectUsernames(null,false,$project_id);
+        $users = '"'.implode('","',$users).'"';
+        $sql = 'SELECT ui_id, username, user_email
+                FROM redcap_user_information 
+                WHERE username IN (?)';
+        $result = $this->query($sql,[$users]); 
+        $projectUsers = [];
+        while($row = $result->fetch_assoc()){
+            $projectUsers[$row['username']] = [
+                'id' => $row['ui_id'],
+                'email' => $row['user_email']
+            ];
+        }
+        
+        // Gather all valid Status IDs for project ID
+        $sql = 'SELECT status_id 
+                FROM redcap_data_quality_status 
+                WHERE project_id = ?';
+        $result = $this->query($sql, [$project_id]);
+        $statusIDs = [];
+        while($row = $result->fetch_assoc()){
+            $statusIDs[] = $row['status_id'];
+        }
+        $statusIDs = implode(',',$statusIDs);
+        
+        // Loop over the user lists and conditionally send emails
+        foreach($settings['user'] as $index => $userList) {
+            $condition = $settings['condition'][$index];
+            $days = $settings['condition'][$index];
+            $freq = $settings['frequency'][$index];
+            $sent = $settings['sent'][$index];
+            
+            if ( empty($condition) || empty($days) || empty($freq) || empty($userList) ) {
+                continue; // We need every setting
+            }
+            
+            if ( $sent != "" && $freq == 0 ) {
+                continue; // Send only once, skip
+            }
+            
+            if ( $sent != "" && $now > date('Y-m-d h:i', strtotime($sent . " + $freq days")) ) {
+                continue; // Not enough time has passed to send the next reminder
+            }
+            
+            // Prep for our query to find open DQs
+            $sql = 'SELECT ts, user_id, comment
+                    FROM redcap_data_quality_resolutions 
+                    WHERE current_query_status = "OPEN" 
+                    AND user_id IN (?) AND status_id IN (?)';
+            $userIds = array_combine(array_keys($projectUsers),array_column($projectUsers, 'id'));
+            $result = [];
+            
+            // If user in the list has open data query
+            if ( $condition == 1 ) {
+                $fUsers = array_values(array_intersect_key($userIds, array_flip($userList)));
+                $result = $this->query($sql,['"'.implode('","',$fUsers).'"',$statusIDs]);
+            }
+            
+            // If any user on the project has an open data query
+            if ( $condition == 2 ) {
+                $result = $this->query($sql,['"'.implode('","',array_values($userIds)).'"',$statusIDs]);
+            }
+            
+            // Check if enough time has passed sense the DQ was opened
+            $sendEmail = false;
+            while($row = $result->fetch_assoc()){
+                if ( $now < date("Y-m-d h:i", strtotime($row['ts'] . " + $days days")) ) {
+                    $sendEmail = true;
+                    break;
+                }
+            }
+            
+            // Send the email and set flag to save
+            if ( $sendEmail ) {
+                foreach ( $userList as $user ) {
+                    $to = $projectUsers[$user]['email'];
+                    $from = $project_contact_email;
+                    $subject = "[REDCap] Data query reminder";
+                    $project_link = "<a link=\"$project_link\">\"$projectName\"</a>";
+                    $msg = "There are open data queries in the REDCap project $project_link that need to be addressed.";
+                    REDCap::email($to, $from, $subject, $msg);
+                }
+                $sentSetting[$index] = $now; 
+                $updateProjectSetting = true;
+            }
+        }
+        
+        // We've flipped through all of the userLits in the project
+        // Update the project setting if anything was sent
+        if ( $updateProjectSetting ) {
+            $this->setProjectSetting("sent",$sentSetting);
+        }
     }
 }
 
